0c0b43f5cb19e3743db8589fd08c5bb325ac7679
Hemraj <hemrajbhatta.pt@gmail.com>
2021-05-21 18:58:05 +0100

    favicon

--- wp-content/themes/lozaizidoro/header.php
+++ wp-content/themes/lozaizidoro/header.php
@@ -12,6 +12,10 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
+    <link rel="icon" sizes="16x16" href="<?php bloginfo('template_url'); ?>/images/favicon-16x16.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?php bloginfo('template_url'); ?>/images/favicon-32x32.png">
+    <link  rel="apple-touch-icon" type="image/png" sizes="180x180" href="<?php bloginfo('template_url'); ?>/images/apple-touch-icon.png">
+
   <meta charset="<?php bloginfo('charset'); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1"/>
   <link rel="profile" href="https://gmpg.org/xfn/11"/>
